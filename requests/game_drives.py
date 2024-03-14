938568694f423755fdc76624ebd452b2c6108809
Tyler Shepherd <100872070+TylerShep@users.noreply.github.com>
2024-03-14 15:41:15 -0600

    Create game_drives.py

---
+++ requests/game_drives.py
@@ -0,0 +1,14 @@
+from data_retriever_service.service import RetrieverService
+from data_pusher_service.psql_service import PusherService
+from data_transformer_service.servicce import DataTransform
+
+# SET PARAMS #
+endpoint = 'drives'
+params =    {"year": 2023,
+             "seasonType": "both",
+             "classification": "fbs"}
+
+data = RetrieverService.getConnection(endpoint, params)
+df_game_drives = DataTransform.responseToDataframe(data)
+df_game_drives = df_game_drives.drop(['offense_conference', 'defense_conference', 'start_time', 'end_time', 'elapsed'], axis=1)
+PusherService.pushToPostgres(df_game_drives, 'd_game_drives')
