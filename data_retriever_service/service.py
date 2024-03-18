cba4a4cdf930cd0afc889ed7bbf279ad0fcd208a
ULTyler ULShepherd <Ltyler.shepherd@rentdynamics.com>
2024-03-18 15:04:26 -0600

    v1.0.4

--- data_retriever_service/service.py
+++ data_retriever_service/service.py
@@ -1,35 +1,35 @@
 import pandas as pd
 import requests
 
-class RetrieverService ():
 
-  def __init__(self) -> None:
-    pass
+class RetrieverService():
 
-  @staticmethod
-  def getCredentials():
-    api_key = 'hVFynv7js27zWixTLvVmLR2u3AetgpOeqbx7Sa/B1RCAij1wcDQkCSIBHcfwFZi1'
-
-    return api_key
-
-  @staticmethod
-  def checkResponseStatus(response):
-    if response.status_code == 200:
+    def __init__(self) -> None:
         pass
-    else:
-        error = response.status_code
-        print(f"Failed to retrieve endpoint: {error}")
 
-  @staticmethod
-  def getConnection(endpoint, parameters):
-    base_url = f'https://api.collegefootballdata.com/{endpoint}'
-    api_key = RetrieverService.getCredentials()
-    headers =   {
-                "Authorization": f'Bearer {api_key}'
-                  }
+    @staticmethod
+    def getCredentials():
+        api_key = 'hVFynv7js27zWixTLvVmLR2u3AetgpOeqbx7Sa/B1RCAij1wcDQkCSIBHcfwFZi1'
 
-    response = requests.get(base_url, headers=headers, params=parameters)
-    response_status = RetrieverService.checkResponseStatus(response)
+        return api_key
 
-    return response.json()
-    
+    @staticmethod
+    def checkResponseStatus(response):
+        if response.status_code == 200:
+            pass
+        else:
+            error = response.status_code
+            print(f"Failed to retrieve endpoint: {error}")
+
+    @staticmethod
+    def getConnection(endpoint, parameters):
+        base_url = f'https://api.collegefootballdata.com/{endpoint}'
+        api_key = RetrieverService.getCredentials()
+        headers = {
+            "Authorization": f'Bearer {api_key}'
+        }
+
+        response = requests.get(base_url, headers=headers, params=parameters)
+        RetrieverService.checkResponseStatus(response)
+
+        return response.json()
--- data_transformer_service/service.py
+++ data_transformer_service/service.py
@@ -6,11 +6,35 @@ class DataTransform():
     if isinstance(response, list):
       df = pd.DataFrame(response)
       df = df.iloc[1:]
+
     else:
       df = pd.read_json(response)
 
     return df
 
-  def dataframeTransformations(df):
-    pass
+  def breakOutListColumns(df: pd.DataFrame) -> pd.DataFrame:
+    list_columns = [col for col in df.columns if df[col].dtype == 'object' and df[col].str.len().mean() > 1]
+
+    dtypes = {col: df[col].dtype for col in list_columns}
+
+    for col in list_columns:
+      df_expanded = df[col].apply(lambda x: pd.Series(x, dtype=dtypes[col])).add_prefix(f'{col}_')
+      df = pd.concat([df, df_expanded], axis=1)
+      df = df.drop(col, axis=1)
+
+    return df
+
+  def remove_suffix_if_exists(df):
+    suffix = '_0'
+    cols = df.columns
+    df.columns = [col.rstrip(suffix) if col.endswith(suffix) else col for col in cols]
+
+    return df
+
+  def dataframeTransform(response):
+    df = DataTransform.responseToDataframe(response)
+    df_breakout = DataTransform.breakOutListColumns(df)
+    df_col_cleanup = DataTransform.remove_suffix_if_exists(df_breakout)
+
+    return df_col_cleanup
     
