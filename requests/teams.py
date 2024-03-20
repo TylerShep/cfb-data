d1c5578aeac683d74cda01ebccacb6f7c4fe1dfe
Tyler Shepherd <100872070+TylerShep@users.noreply.github.com>
2024-03-20 12:25:33 -0600

    v1.0.10

--- requests/teams.py
+++ requests/teams.py
@@ -1,6 +1,6 @@
 from data_retriever_service.service import RetrieverService
 from data_pusher_service.postgres_service import PusherService
-from data_transformer_service.service import DataTransform
+from data_transformer_service.service import TransformService
 
 
 class TeamsRequestService():
@@ -10,7 +10,7 @@ class TeamsRequestService():
         params = {}
 
         data = RetrieverService.getPostgresConnection(endpoint, params)
-        df = DataTransform.dataframeTransform(data)
+        df = TransformService.dataframeTransform(data)
         table_name = PusherService.createPostgresTable(df, endpoint)
 
         PusherService.pushToPostgres(df, table_name)
